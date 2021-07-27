29cb27f95291608cf46d5f168af7312b3de83739
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2021-07-26 19:39:03 -0600

    Primeros avances

---
+++ BaseDeDatos/limpieza.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Zephyrus/Documents/U/8vo Semestre/DataScience/DataScience-Project1/BaseDeDatos/csv")
+
+altaV <- read.csv("AltaVerapaz.csv")
+guatemala <- read.csv("Guatemala.csv")
+View(guatemala)
